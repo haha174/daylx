255394c0b5342f11cbdb1fe0535adfebfbee98b8
Aaron <https://github.com/haha174/daylx.git>
2019-10-08 13:18:06 +0800

    permutations

--- src/sum/Main.java
+++ src/sum/Main.java
@@ -2,8 +2,8 @@ package sum;
 
 public class Main {
     public static void main(String args[]) {
-        int[] nums = {0, 2, 7, 11, 15};
-        int target = 15;
-        System.out.println(new Solution().twoSum(nums, target));
+        final int key;
+        key=2;
+        System.out.println(key);
     }
 }
