a94002ef5e66514a31ee57b3200588b7d433e2c3
wenqchen <172839Zxc>
2020-01-27 12:41:53 +0800

    https://leetcode.com/problems/isomorphic-strings/submissions/

--- src/isomorphic_strings/Solution.java
+++ src/isomorphic_strings/Solution.java
@@ -26,10 +26,34 @@ package isomorphic_strings;
 class Solution {
     public boolean isIsomorphic(String s, String t) {
 
-        for (int i = 0; i < s.length(); i++) {
-            if (s.indexOf(s.charAt(i)) != t.indexOf(t.charAt(i))) return false;
+//        for (int i = 0; i < s.length(); i++) {
+//            if (s.indexOf(s.charAt(i)) != t.indexOf(t.charAt(i))) return false;
+//        }
+//        return true;
+        char[] map = new char[128];
+        boolean[] used = new boolean[128];
+
+        char[] chs1 = s.toCharArray();
+        char[] chs2 = t.toCharArray();
+
+        if(chs1.length != chs2.length) return false;
+
+        for(int i=chs1.length-1 ; i>=0 ; i--){
+            if(map[chs1[i]] == chs2[i]) {
+                continue;
+            }
+
+            if(map[chs1[i]] == '\0' && !used[chs2[i]]){
+                map[chs1[i]] = chs2[i];
+                used[chs2[i]] = true;
+            }
+            else {
+                return false;
+            }
         }
         return true;
+
+
     }
 
     public static void main(String[] args) {
