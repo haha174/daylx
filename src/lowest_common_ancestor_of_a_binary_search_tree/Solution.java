45a9381cb134f2af582f55824232d7d0674a2a25
wenqchen <172839Zxc>
2020-02-02 12:26:06 +0800

    https://leetcode.com/submissions/detail/299462664/

--- src/lowest_common_ancestor_of_a_binary_search_tree/Solution.java
+++ src/lowest_common_ancestor_of_a_binary_search_tree/Solution.java
@@ -30,8 +30,17 @@ class TreeNode {
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root==null || (root.val>=p.val && root.val<=q.val) || (root.val>=q.val && root.val<=p.val))return root;
-        else if (root.val <= p.val && root.val<=q.val)return lowestCommonAncestor(root.right,p,q);
-        else return lowestCommonAncestor(root.left,p,q);
+//        if (root==null || (root.val>=p.val && root.val<=q.val) || (root.val>=q.val && root.val<=p.val))return root;
+//        else if (root.val <= p.val && root.val<=q.val)return lowestCommonAncestor(root.right,p,q);
+//        else return lowestCommonAncestor(root.left,p,q);
+        TreeNode current = root;
+        while(true){
+            if(current.val > p.val && current.val > q.val)
+                current = current.left;
+            else if(current.val < p.val && current.val < q.val)
+                current = current.right;
+            else break;
+        }
+        return current;
     }
 }
