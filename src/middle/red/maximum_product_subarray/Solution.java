ebf85fac58a070004a62f5da485d0dc8adb90f99
haha174 <cwqxhh@163.com>
2022-05-17 08:18:20 +0800

    944. 删列造序

---
+++ src/middle/red/maximum_product_subarray/Solution.java
@@ -0,0 +1,15 @@
+package middle.red.maximum_product_subarray;
+class Solution {
+    public int maxProduct(int[] nums) {
+        int maxF = nums[0], minF = nums[0], ans = nums[0];
+        int length = nums.length;
+        for (int i = 1; i < length; ++i) {
+            int mx = maxF, mn = minF;
+            maxF = Math.max(mx * nums[i], Math.max(nums[i], mn * nums[i]));
+            minF = Math.min(mn * nums[i], Math.min(nums[i], mx * nums[i]));
+            ans = Math.max(maxF, ans);
+        }
+        return ans;
+    }
+}
+
