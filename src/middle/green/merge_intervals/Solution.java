416c732b3e7d87bb79af111d2047c283b1bf0a0a
diaobao <cwqxhxh@163.com>
2024-04-28 22:44:53 +0800

    s

--- src/middle/green/merge_intervals/Solution.java
+++ src/middle/green/merge_intervals/Solution.java
@@ -7,30 +7,36 @@ import java.util.List;
 
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if (o1[0]==o2[0]){
-                   return   Integer.compare(o1[1],o2[1]);
-                }
-                return Integer.compare(o1[0],o2[0]);
-            }
-        });
-
-        List<int[]> merged = new ArrayList<int[]>();
-        for (int i = 0; i < intervals.length; ++i) {
-            int L = intervals[i][0], R = intervals[i][1];
-            if (merged.size() == 0 || merged.get(merged.size() - 1)[1] < L) {
-                merged.add(new int[]{L, R});
-            } else {
-                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], R);
-            }
-        }
-        return merged.toArray(new int[merged.size()][]);
+       Arrays.sort(intervals, new Comparator<int[]>() {
+           @Override
+           public int compare(int[] o1, int[] o2) {
+               if (o1[0]==o2[0]){
+                   return o1[1]-o2[1];
+               }else {
+                   return o1[0]-o2[0];
+               }
+           }
+       });
+       List<int[]> result=new ArrayList<>();
+       int[] pre=intervals[0];
+       for (int i=1;i<intervals.length;i++){
+           int[] tmp=intervals[i];
+           if (tmp[0]<=pre[1]){
+               pre[1]=Math.max( pre[1],tmp[1]);
+           }else {
+               result.add(pre);
+               pre=tmp;
+           }
+       }
+       if (pre!=null){
+           result.add(pre);
+       }
+       return result.toArray(new int[result.size()][2]);
     }
 
     public static void main(String[] args) {
         int[][] intervals = {{1,3},{2,6},{8,10},{15,18},{2,3}};
-        System.out.println(new Solution().merge(intervals));
+        int[][] result =  new Solution().merge(intervals);
+        System.out.println(result);
     }
 }
