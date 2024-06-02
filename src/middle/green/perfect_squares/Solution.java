451226bf18031350f66d202dc624378052eff028
diaobao <cwqxhxh@163.com>
2024-06-02 11:15:29 +0800

    s

--- src/middle/green/perfect_squares/Solution.java
+++ src/middle/green/perfect_squares/Solution.java
@@ -2,15 +2,16 @@ package middle.green.perfect_squares;
 
 class Solution {
     public int numSquares(int n) {
-        int[] f = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            int minn = Integer.MAX_VALUE;
-            for (int j = 1; j * j <= i; j++) {
-                minn = Math.min(minn, f[i - j * j]);
+        int[] dp=new int[n+1];
+        dp[1]=1;
+        for (int i=2;i<=n;i++){
+            int min=Integer.MAX_VALUE;
+            for (int j=1;j*j<=i;j++){
+                min=Math.min(min,dp[i-j*j]);
             }
-            f[i] = minn + 1;
+            dp[i]=min+1;
         }
-        return f[n];
+        return dp[n];
     }
 
     public static void main(String[] args) {
